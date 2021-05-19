e2be20180cea66839d072e3c4355a3c4ed0f2778
kimjunho <wnsghi1@naver.com>
2021-05-19 21:11:13 +0900

    RESTful Web Service

---
+++ src/main/java/com/springbootguide/springbootguide/SpringbootguideApplication.java
@@ -0,0 +1,13 @@
+package com.springbootguide.springbootguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootguideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootguideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbootguide/springbootguide/SpringbootguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootguide.springbootguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
