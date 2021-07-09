936b3773a4f57a3822abdb79703db612e1344f26
kimjunho <wnsghi1@naver.com>
2021-07-10 00:02:10 +0900

    Springboot Guide - RESTful Web Service

--- src/main/java/com/springbootguide/springbootguide/SpringbootguideApplication.java
+++ src/main/java/com/springbootguide/springbootguide/SpringbootguideApplication.java
@@ -1,13 +1,36 @@
 package com.springbootguide.springbootguide;
 
+import com.springbootguide.springbootguide.comsuming_restful_web_service.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringbootguideApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringbootguideApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootguideApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        System.out.println("[resttemaplte !]");
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        System.out.println("[run!] : " + restTemplate);
+        return args -> {
+            Quote quote = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
 }
---
+++ src/main/java/com/springbootguide/springbootguide/comsuming_restful_web_service/Value.java
@@ -0,0 +1,36 @@
+package com.springbootguide.springbootguide.comsuming_restful_web_service;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Value {
+
+    private Long id;
+    private String quote;
+
+    public Value() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
+}
